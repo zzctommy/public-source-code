70ee3f1d6ee8ddcf9d7e2cc6a7872c38ff8c5058
zzctommy <67889443+zzctommy@users.noreply.github.com>
2020-07-07 19:29:23 +0800

    Update CF1288F Red-Blue Graph.cpp

--- Luogu/CF1288F Red-Blue Graph.cpp
+++ Luogu/CF1288F Red-Blue Graph.cpp
@@ -12,14 +12,16 @@ inline int rd() {
 	while(isdigit(ch))x=x*10+(ch^48),ch=getchar();
 	return x*f;
 }
-const int N=5010;
-const int M=100010;
+const int N=410;
+const int M=3000;
 const int inf=1<<28;
 int head[N],num_edge=1;
-int n,m,s,t;
-int maxflow,cost;
-int inq[N],dst[N],cur[N];
+int n1,n2,m,r,b;
+int s,t,S,T,SS,TT,Begin,Final;
+int maxflow,sumflow,cost,ans;
+int dta[N],inq[N],dst[N],cur[N];
 bool vis[N];
+char str[N];
 struct edge {
 	int nxt,to,val,w;
 }e[M];
@@ -31,8 +33,12 @@ void addedge(int from,int to,int val,int w) {
 	e[num_edge].w=w;
 	head[from]=num_edge;
 }
+void addedge(int from,int to,int l,int r,int w) {
+	addedge(from,to,r-l,w),addedge(to,from,0,-w);
+	dta[from]-=l,dta[to]+=l;
+}
 bool spfa() {
-	for(rint i=1;i<=n;++i)cur[i]=head[i],inq[i]=0,dst[i]=inf;
+	for(rint i=1;i<=TT;++i)cur[i]=head[i],inq[i]=0,dst[i]=inf;
 	queue<int>q;q.push(s);dst[s]=0;
 	while(!q.empty()) {
 		int u=q.front();inq[u]=0,q.pop();
@@ -66,19 +72,43 @@ int dfs(int u,int flow) {
 	return used;
 }
 void MCMF() {
-	maxflow=cost=0;
 	while(spfa()) {
 		vis[t]=1;
 		while(vis[t]){memset(vis,0,sizeof(vis)),dfs(s,inf);}
 	}
 }
 signed main() {
-	n=rd(),m=rd(),s=rd(),t=rd();
-	for(rint i=1;i<=m;++i) {
-		int x=rd(),y=rd(),z=rd(),w=rd();
-		addedge(x,y,z,w),addedge(y,x,0,-w);
+	n1=rd(),n2=rd(),m=rd(),r=rd(),b=rd();
+	S=n1+n2+1,T=S+1,SS=T+1,TT=SS+1;
+	scanf("%s",str+1);
+	for(rint i=1;i<=n1;++i) {
+		if(str[i]=='R')addedge(S,i,1,inf,0);
+		else if(str[i]=='B')addedge(i,T,1,inf,0);
+		else addedge(S,i,0,inf,0),addedge(i,T,0,inf,0);
 	}
-	MCMF();
-	cout<<maxflow<<' '<<cost<<endl;
+	scanf("%s",str+1);
+	for(rint i=1;i<=n2;++i) {
+		if(str[i]=='B')addedge(S,i+n1,1,inf,0);
+		else if(str[i]=='R')addedge(i+n1,T,1,inf,0);
+		else addedge(S,i+n1,0,inf,0),addedge(i+n1,T,0,inf,0);
+	}
+	Begin=num_edge+1;
+	for(rint i=1;i<=m;++i) {
+		int x=rd(),y=rd();
+		addedge(x,y+n1,0,1,r),addedge(y+n1,x,0,1,b);
+	}
+	Final=num_edge;
+	addedge(T,S,0,inf,0);
+	for(rint i=1;i<=T;++i)
+		if(dta[i]>0)addedge(SS,i,0,dta[i],0),sumflow+=dta[i];
+		else addedge(i,TT,0,-dta[i],0);
+	s=SS,t=TT,MCMF();
+	if(maxflow!=sumflow)return puts("-1"),0;
+	printf("%d\n",cost);
+	for(rint i=Begin;i<=Final;i+=4) {
+		if(!e[i].val)putchar('R');
+		else if(!e[i+2].val)putchar('B');
+		else putchar('U');
+	}
 	return 0;
 }
